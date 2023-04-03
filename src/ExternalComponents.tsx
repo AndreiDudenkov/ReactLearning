639bffc2ec4a5002bb459214324525ba72ffe859
AndreiDudenkov <andrei634d@gmail.com>
2023-04-03 23:49:42 +0300

    filter + useState + css

--- src/ExternalComponents.tsx
+++ src/ExternalComponents.tsx
@@ -1,37 +1,41 @@
 import React from "react";
 
-type NewComponentType ={
+type NewComponentType = {
     currentMoneyPr: Array<MoneyType>
-    callback: (name: string)=> void
+    callback: (name: string) => void
 }
-type MoneyType= {
+type MoneyType = {
     banknote: string,
     nominal: number,
     number: string
 }
 
-export const NewComponent = (props:NewComponentType) => {
+export const NewComponent = (props: NewComponentType) => {
 
-    const onClickHandler = (param:string) => {
-        {props.callback(param)}
+    const onClickHandler = (param: string) => {
+        {
+            props.callback(param)
+        }
     }
 
     return (
-<div id='body'>
-    <ul>
-        {props.currentMoneyPr.map((objFromMoneyArr, index) => {
-            return (
-                <li key={index}>
-                    <span>{objFromMoneyArr.banknote}</span>
-                    <span>{objFromMoneyArr.nominal}</span>
-                    <span>{objFromMoneyArr.number}</span>
-                </li>
-            )
-        })}
-    </ul>
-    <button onClick={()=>onClickHandler('all')}>all</button>
-    <button onClick={()=>onClickHandler('dollar')}>dollar</button>
-    <button onClick={()=>onClickHandler('ruble')}>ruble</button>
-</div>
-)
+        <div id='body'>
+            <ul>
+                {props.currentMoneyPr.map((objFromMoneyArr, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{objFromMoneyArr.banknote}</span>
+                            <span>{objFromMoneyArr.nominal}</span>
+                            <span>{objFromMoneyArr.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div id='buttons'>
+                <button onClick={() => onClickHandler('all')}>all</button>
+                <button onClick={() => onClickHandler('dollar')}>dollar</button>
+                <button onClick={() => onClickHandler('ruble')}>ruble</button>
+            </div>
+        </div>
+    )
 }
